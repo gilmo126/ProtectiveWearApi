966e1a66819bc4bcc5ec24c516e1af76ca27e53d
guillaumer gil moreno <yiyo26@outlook.es>
2020-05-23 10:31:47 -0500

    Se agrega identityUser y el JWT para permitir ingresos autorizados. Se integran funciones al controlador Account para el registro e identificacion del usuario.

---
+++ ProtectiveWearSecurity/Models/ProtectiveWearApiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProtectiveWearSecurity.Models;
+
+namespace ProtectiveWearSecurity.Models
+{
+    public class ProtectiveWearApiDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ProtectiveWearApiDbContext(DbContextOptions<ProtectiveWearApiDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public ProtectiveWearApiDbContext() { 
+        }
+    }
+}
---
+++ ProtectiveWearSecurity/Models/RegisterApiResource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProtectiveWearSecurity.Models
+{
+    public class RegisterApiResource
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "El {0} debe estar entre {2} and un maximo {1} caracteres de largo.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "La confirmacion del password no coinciden.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
